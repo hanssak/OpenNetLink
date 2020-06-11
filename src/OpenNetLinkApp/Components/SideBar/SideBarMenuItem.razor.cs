50388b301c5d795fc367e7f3d1840d36dc4864d1
angellos12 <angellos124@gmail.com>
2020-06-11 20:22:52 +0900

    * SideBar Menu 컴포넌트 구현  및 기능추가

---
+++ src/OpenNetLinkApp/Components/SideBar/SideBarMenuItem.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OpenNetLinkApp.Components.SideBar
+{
+    public class SideBarMenuItemBase : ComponentBase
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public bool IsEnabled { get; set; } = true;
+        [Parameter] public bool IsVisible { get; set; } = true;
+        [Parameter] public string Css { get; set; } = string.Empty;
+        [Parameter] public string Link { get; set; } = string.Empty;
+        [Parameter] public string Title { get; set; } = string.Empty;
+        [Parameter] public string Icon { get; set; } = string.Empty;
+        [Parameter] public string Badge { get; set; } = string.Empty;
+        [Parameter] public string BadgeValue { get; set; } = string.Empty;
+        [Parameter] public MenuItem MenuItem { get; set; }
+
+        protected string CssString
+        {
+            get
+            {
+                var cssString = string.Empty;
+
+                cssString += $"{Css}";
+                cssString += !IsEnabled ? " disabled" : "";
+                cssString += !IsVisible ? " hidden" : "";
+
+                return cssString.Trim();
+            }
+        }
+    }
+}
---
+++ src/OpenNetLinkApp/Components/SideBar/SideBarSubMenu.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Collections.Generic;
+
+namespace OpenNetLinkApp.Components.SideBar
+{
+    public class SideBarSubMenuBase : ComponentBase
+    {
+        [Parameter] public string Css { get; set; }
+        [Parameter] public string Header { get; set; }
+        [Parameter] public string Icon { get; set; }
+        [Parameter] public string Badge { get; set; }
+        [Parameter] public string BadgeValue { get; set; }
+        [Parameter] public bool IsEnabled { get; set; } = true;
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public RenderFragment HeaderTemplate { get; set; }
+        [Parameter] public RenderFragment MenuTemplate { get; set; } 
+        [Parameter] public IEnumerable<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+
+        protected string LastIcon { get; set; } = "+";
+        protected bool IsOpen { get; set; }
+        protected string CssString
+        {
+            get
+            {
+                var cssString = "";
+
+                cssString += $" {Css}";
+                cssString += IsOpen ? " open" : "";
+
+                return cssString.Trim();
+            }
+        }
+
+        protected void ToggleSubMenu()
+        {
+            IsOpen = !IsOpen;
+            LastIcon = IsOpen ? "-" : "+";
+        }
+
+        /// <summary>
+        /// Handler for the key down events
+        /// </summary>
+        /// <param name="eventArgs">keyboard event</param>
+        protected void KeyDownHandler(KeyboardEventArgs eventArgs)
+        {
+            if (eventArgs.Key == "Enter" || eventArgs.Key == " " || eventArgs.Key == "Spacebar")
+            {
+                ToggleSubMenu();
+            }
+        }
+
+    }
+}
