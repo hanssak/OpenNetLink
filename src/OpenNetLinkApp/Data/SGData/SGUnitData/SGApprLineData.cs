c93665bfe77a3094c3b3f471e552a177b03526ef
KangShin <battledir@hanssak.co.kr>
2020-07-21 22:09:27 +0900

    1. User 데이터 클래스 결재권,보안결재, 이름, 부서명 정보를 얻어오는 함수 구현. 2. And 기본 결재라인 수신 후 데이터 파싱이 가능하도록 함수 구현.

--- src/OpenNetLinkApp/Data/SGData/SGUnitData/SGApprLineData.cs
+++ src/OpenNetLinkApp/Data/SGData/SGUnitData/SGApprLineData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using HsNetWorkSGData;
+using OpenNetLinkApp.Pages.Exam.Pages;
 
 namespace OpenNetLinkApp.Models.Data
 {
@@ -19,6 +20,44 @@ namespace OpenNetLinkApp.Models.Data
         {
             m_DicTagData = new Dictionary<string, string>(data.m_DicTagData);
             m_DicRecordData = new List<Dictionary<int, string>>(data.m_DicRecordData);
+        }
+
+        public List<string> GetApprAndLineName()
+        {
+            List<string> listApprLine = new List<string>();
+            List<Dictionary<int, string>> listDicdata = GetRecordData("APPROVERECORD");
+            int nTotalCount = listDicdata.Count;
+            for(int i=0;i<nTotalCount;i++)                              // UI 에서 사용하기 위해 자기 자신을 포함하기 위해 i = 0 부터 시작.                  
+            {
+                Dictionary<int, string> dic = listDicdata[i];
+                string tmpStr = "";
+                if (dic.TryGetValue(2, out tmpStr) == true)
+                {
+                    tmpStr = dic[2];
+                    if(!tmpStr.Equals(""))
+                        listApprLine.Add(tmpStr);
+                }
+            }
+            return listApprLine;
+        }
+
+        public List<string> GetApprAndLineSeq()
+        {
+            List<string> listApprLine = new List<string>();
+            List<Dictionary<int, string>> listDicdata = GetRecordData("APPROVERECORD");
+            int nTotalCount = listDicdata.Count;
+            for (int i = 1; i < nTotalCount; i++)                       // 파일 전송 시 사용하기 위해 자기 자신을 제외하기 위해서 i = 1 부터 시작.
+            {
+                Dictionary<int, string> dic = listDicdata[i];
+                string tmpStr = "";
+                if (dic.TryGetValue(0, out tmpStr) == true)
+                {
+                    tmpStr = dic[0];
+                    if (!tmpStr.Equals(""))
+                        listApprLine.Add(tmpStr);
+                }
+            }
+            return listApprLine;
         }
     }
 }
