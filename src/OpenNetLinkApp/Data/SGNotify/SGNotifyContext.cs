d3fbd23e403b8c1b9955bd26ae1105e6d7e028db
angellos12 <angellos124@gmail.com>
2020-07-27 14:57:11 +0900

    Add SQLITE3 SGNotifyDB and DBContext and DBModeling

--- src/OpenNetLinkApp/Data/SGNotify/SGNotifyContext.cs
+++ src/OpenNetLinkApp/Data/SGNotify/SGNotifyContext.cs
@@ -1,5 +1,6 @@
+using System.Reflection.Emit;
 using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using OpenNetLinkApp.Models.SGNotify;
 
@@ -19,8 +20,42 @@ namespace OpenNetLinkApp.Data.SGNotify
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<SGAlarmData>().ToTable("T_SG_ALARM");
-            modelBuilder.Entity<SGNotiData>().ToTable("T_SG_NOTI");
+            base.OnModelCreating(modelBuilder);
+
+            /* TABLE: SGAlarmData */
+            modelBuilder.Entity<SGAlarmData>()
+                        .ToTable("T_SG_ALARM");
+            modelBuilder.Entity<SGAlarmData>()
+                        .Property(c => c.AlarmId).IsRequired();
+            modelBuilder.Entity<SGAlarmData>()
+                        .HasKey(k => new { k.AlarmId, k.GroupId }).HasName("PK_T_SG_ALARM_ALARMID_GROUPID");
+            modelBuilder.Entity<SGAlarmData>()
+                        .Property(c => c.Path).HasColumnType("varchar(64)");
+            modelBuilder.Entity<SGAlarmData>()
+                        .Property(c => c.IconImage).HasColumnType("varchar(128)");
+            modelBuilder.Entity<SGAlarmData>()
+                        .Property(c => c.Head).HasColumnType("varchar(64)").IsRequired();
+            modelBuilder.Entity<SGAlarmData>()
+                        .Property(c => c.Body).HasColumnType("varchar(255)").IsRequired();
+            modelBuilder.Entity<SGAlarmData>()
+                        .Property(c => c.Time).HasColumnType("TEXT").HasDefaultValueSql("GETDATE()").IsRequired();
+            /* TABLE: SGNotiData */
+            modelBuilder.Entity<SGNotiData>()
+                        .ToTable("T_SG_NOTI");
+            modelBuilder.Entity<SGNotiData>()
+                        .Property(c => c.NotiId).IsRequired();
+            modelBuilder.Entity<SGNotiData>()
+                        .HasKey(k => new { k.NotiId, k.GroupId }).HasName("PK_T_SG_NOTI_NOTIID_GROUPID");
+            modelBuilder.Entity<SGNotiData>()
+                        .Property(c => c.Path).HasColumnType("varchar(64)");
+            modelBuilder.Entity<SGNotiData>()
+                        .Property(c => c.IconImage).HasColumnType("varchar(128)");
+            modelBuilder.Entity<SGNotiData>()
+                        .Property(c => c.Head).HasColumnType("varchar(64)").IsRequired();
+            modelBuilder.Entity<SGNotiData>()
+                        .Property(c => c.Body).HasColumnType("varchar(255)").IsRequired();
+            modelBuilder.Entity<SGNotiData>()
+                        .Property(c => c.Time).HasColumnType("TEXT").HasDefaultValueSql("GETDATE()").IsRequired();
         }
     }
 }
---
+++ src/OpenNetLinkApp/Migrations/20200727055345_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OpenNetLinkApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "T_SG_ALARM",
+                columns: table => new
+                {
+                    AlarmId = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GroupId = table.Column<int>(nullable: false),
+                    Path = table.Column<string>(type: "varchar(64)", nullable: true),
+                    IconImage = table.Column<string>(type: "varchar(128)", nullable: true),
+                    Head = table.Column<string>(type: "varchar(64)", nullable: false),
+                    Body = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Time = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_SG_ALARM_ALARMID_GROUPID", x => new { x.AlarmId, x.GroupId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "T_SG_NOTI",
+                columns: table => new
+                {
+                    NotiId = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GroupId = table.Column<int>(nullable: false),
+                    Path = table.Column<string>(type: "varchar(64)", nullable: true),
+                    IconImage = table.Column<string>(type: "varchar(128)", nullable: true),
+                    Head = table.Column<string>(type: "varchar(64)", nullable: false),
+                    Body = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Time = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_SG_NOTI_NOTIID_GROUPID", x => new { x.NotiId, x.GroupId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "T_SG_ALARM");
+
+            migrationBuilder.DropTable(
+                name: "T_SG_NOTI");
+        }
+    }
+}
