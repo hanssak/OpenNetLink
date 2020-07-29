8e535ee2240d3e150a0a0a4be69bc0f20070e992
angellos12 <angellos124@gmail.com>
2020-07-29 15:38:44 +0900

    IMP PageName and User Info and Notice of HeaderUI (1th fix)

--- src/OpenNetLinkApp/Models/SGHeader/SGAlarmInfoUI.cs
+++ src/OpenNetLinkApp/Models/SGHeader/SGAlarmInfoUI.cs
@@ -5,8 +5,8 @@ namespace OpenNetLinkApp.Models.SGHeader
 {
     public class SGAlarmInfoUI
     {
-        public string AlarmPath { get; private set; } = String.Empty;
-        public string Icon { get; private set; } = String.Empty;
-        public string Badge { get; private set; } = String.Empty;
+        public string AlarmPath { get; private set; } = "";
+        public string Icon { get; private set; } = "fas fa-bell";
+        public string Badge { get; private set; } = "badge badge-warning";
     }
 }
