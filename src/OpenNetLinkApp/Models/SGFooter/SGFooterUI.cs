bbd7f50e2199914cd0b133bed7b0fe3bf5967da7
angellos12 <angellos124@gmail.com>
2020-07-31 17:27:54 +0900

    Add SGFooterUIService Implements(Not yet written Copy Right Info)

--- src/OpenNetLinkApp/Models/SGFooter/SGFooterUI.cs
+++ src/OpenNetLinkApp/Models/SGFooter/SGFooterUI.cs
@@ -7,10 +7,11 @@ namespace OpenNetLinkApp.Models.SGFooter
     {
         public SGFooterUI()
         {
+            Description = new List<string>();
             Description.Add("(주) 한싹시스템");
             Description.Add("서울시 구로구 디지털로34길 27 401호-403호 (구로동, 대륭포스트타워 3차)");
             Description.Add("COPYRIGHT 2020 HANSSAK. ALL RIGHTS RESERVED");
         }
-        public List<string> Description { get; private set;}
+        public List<string> Description { get; private set;} = null;
     }
 }
