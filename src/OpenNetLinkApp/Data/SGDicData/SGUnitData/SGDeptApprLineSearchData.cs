33c886b46d86ce48d8b947b22f85e89ff9a004d4
KangShin <battledir@hanssak.co.kr>
2020-08-05 20:57:48 +0900

    부서 결재자 정보를 저장하는 데이터 클래스 추가

---
+++ src/OpenNetLinkApp/Data/SGDicData/SGUnitData/SGDeptApprLineSearchData.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HsNetWorkSGData;
+using HsNetWorkSG;
+
+namespace OpenNetLinkApp.Data.SGDicData.SGUnitData
+{
+    public class SGDeptApprLineSearchData : SGData
+    {
+        public SGDeptApprLineSearchData()
+        {
+
+        }
+        ~SGDeptApprLineSearchData()
+        {
+
+        }
+        public void Copy(HsNetWork hs, SGData data)
+        {
+            SetSessionKey(hs.GetSeedKey());
+            m_DicTagData = new Dictionary<string, string>(data.m_DicTagData);
+            m_DicRecordData = new List<Dictionary<int, string>>(data.m_DicRecordData);
+        }
+    }
+}
