6565627087ed1d48aa8ade2115c37a8a38f75206
angellos12 <angellos124@gmail.com>
2020-08-13 16:46:05 +0900

    Add Preview feature (PreviewUtil and Call Preview OpenWindow)

---
+++ src/PreviewUtil/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using WebWindows;
+
+namespace PreviewUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = new WebWindow("   PreviewUtilApp", options =>
+            {
+                options.SchemeHandlers.Add("app", (string url, out string contentType) =>
+                {
+                    contentType = "text/javascript";
+                    return new MemoryStream(Encoding.UTF8.GetBytes(""));
+                });
+            });
+
+            window.OnWebMessageReceived += (sender, message) =>
+            {
+                window.SendMessage("Got message: " + message);
+            };
+
+            Console.WriteLine("0: {0}", args[0]);
+            window.NavigateToUrl(args[0]);
+            window.WaitForExit();
+        }
+    }
+}
