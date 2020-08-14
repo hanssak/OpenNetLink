91b32c1f7c381416947c6faf3d930f5e6fc67ff8
uglyice <uglyice@naver.com>
2020-08-14 14:57:02 +0900

    사용자 정보 화면

--- src/OpenNetLinkApp/Data/SGDicData/SGUnitData/SGUserData.cs
+++ src/OpenNetLinkApp/Data/SGDicData/SGUnitData/SGUserData.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using HsNetWorkSGData;
 using HsNetWorkSG;
+using OpenNetLinkApp.Services;
 
 namespace OpenNetLinkApp.Data.SGDicData.SGUnitData
 {
     public class SGUserData : SGData
     {
+        private readonly XmlConfService xconf = new XmlConfService();
         public SGUserData()
         {
 
@@ -42,6 +44,29 @@ namespace OpenNetLinkApp.Data.SGDicData.SGUnitData
             int nApprPos = Convert.ToInt32(strData);
             return nApprPos;
         }
+        // GetUserApprpos 확장함수로 문자열을 직접 제공한다
+        public string GetUserApprposString()
+        {
+            string stRtn = String.Empty;
+            string strData = GetUserTagData("APPRPOS");
+            int nApprPos = Convert.ToInt32(strData);
+            switch(nApprPos)
+            {
+                case 0:
+                    stRtn = xconf.GetTitle("T_INFO_NORMAL_USER");
+                    break;
+                case 1:
+                    stRtn = xconf.GetTitle("T_INFO_APPROVER_USER");
+                    break;
+                case 2:
+                    stRtn = xconf.GetTitle("T_INFO_APPROVE_FREE_USER");
+                    break;
+                default:
+                    stRtn = "";
+                    break;
+            }
+            return stRtn;
+        }
         /**
         *@biref 사용자의 보안결재권한 여부를 반환한다.
         *@return true : 보안결재자
@@ -53,6 +78,16 @@ namespace OpenNetLinkApp.Data.SGDicData.SGUnitData
             if (iValue == 1)
                 return true;
             return false;
+        }
+        // GetUserPrivacyApprPos 확장함수 1:보안결재자 기타:일반사용자
+        public string GetUserPrivacyApprPosString()
+        {
+            string strData = GetUserTagData("DLPAPPRPOS");
+            int iValue = Convert.ToInt32(strData);
+            if (iValue == 1)
+                return xconf.GetTitle("T_APPROVE_PRIVACY");
+            else
+                return xconf.GetTitle("T_INFO_NORMAL_USER");
         }
         /**
         *@biref 사용자의 이름을 반환한다.
