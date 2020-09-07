8b1c49f68c284c421ca69511b65dd325143152fb
jkkim-open <jkkim@hanssak.co.kr>
2020-09-07 17:05:47 +0900

    스크립트 수정

--- src/build.linux.simple.sh
+++ src/build.linux.simple.sh
@@ -11,4 +11,7 @@ mkdir -p  ${TOPF}/${APPF}
 dotnet clean ;
 dotnet build -c Release -o  ${TOPF}/${APPF}
 
+cp bin.Script/OpenNetLinkApp.sh ${TOPF}/${APPF}
+chmod 755 ${TOPF}/${APPF}/OpenNetLinkApp.sh
+
 tar cvf bin.${DATE}.tar  ./${TOPF}
