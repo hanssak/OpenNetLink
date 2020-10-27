72062d272fdab2de140595fe9090dd4e5877eba1
uglyice <uglyice@naver.com>
2020-10-27 16:12:19 +0900

    zip 팝업 추가

--- src/OpenNetLinkApp/wwwroot/js/DragAndDrop.js
+++ src/OpenNetLinkApp/wwwroot/js/DragAndDrop.js
@@ -655,6 +655,7 @@ window.closeAllPopup = () => {
 	$("#ApproverSelect_TreePopUp").modal("hide");
 	$("#ReceiverSelect_PopUp").modal("hide");
 	$("#TransPopUp").modal("hide");
+	$("#ZipPreviewPopUp").modal("hide");
 }
 
 window.initApproveUI = () => {
