523ef0c82e7a7627ee5c326e212439c2a6f3f480
uglyice <uglyice@naver.com>
2020-12-14 18:37:01 +0900

    UI 간격 수정

--- src/OpenNetLinkApp/wwwroot/js/DragAndDrop.js
+++ src/OpenNetLinkApp/wwwroot/js/DragAndDrop.js
@@ -673,18 +673,30 @@ window.closeAllPopup = () => {
 window.initApproveUI = () => {
 	
 	$("#datepicker").datepicker({
+		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
 		autoclose: true,
 		dateFormat: 'yy-mm-dd'
 	})
 	$("#datepicker2").datepicker({
+		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
 		autoclose: true,
 		dateFormat: "yy-mm-dd"
 	})
 	$("#datepicker3").datepicker({
+		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
 		autoclose: true,
 		dateFormat: "yy-mm-dd"
 	})
 	$("#datepicker4").datepicker({
+		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
 		autoclose: true,
 		dateFormat: "yy-mm-dd"
 	})
