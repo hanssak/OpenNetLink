abeae0d1b6a2618368433e23d8a66f98d182177c
angellos12 <angellos124@gmail.com>
2021-02-15 19:10:55 +0900

    Chg Add Build System for MacOS App Building

---
+++ src/MacOSAppLayout/PkgAndNotarize.sh
@@ -0,0 +1,179 @@
+#!/bin/zsh
+
+# pkgAndNotarize.sh
+
+# 2019 - Armin Briegel - Scripting OS X
+
+# place a copy of this script in in the project folder
+# when run it will build for installation,
+# create a pkg from the product,
+# upload the pkg for notarization and monitor the notarization status
+
+# before you can run this script:
+# - set release signing of the tool to 'Developer ID Application'
+# - enable the hardened run-time
+# - change the 'Installation Build Products Location' to `$SRCROOT/build/pkgroot`
+# 
+# you want to add the `build` subdirectory to gitignore
+
+
+# put your dev account information into these variables
+
+# the email address of your developer account
+dev_account="angellos124@gmail.com"
+
+# the name of your Developer ID installer certificate
+# signature="Developer ID Installer: First Last (ABCD123456)"
+signature="Developer ID Installer: Hanssak System Co., Ltd. (L7W5N48H4G)"
+# appledevsig="Apple Development: 명훈 민 (NY3NHU3Q3D)"
+appledevsig="Developer ID Application: Hanssak System Co., Ltd. (L7W5N48H4G)"
+
+# the 10-digit team id
+# dev_team="ABCD123456"
+dev_team="L7W5N48H4G"
+
+# the label of the keychain item which contains an app-specific password
+# dev_keychain_label="Developer-altool"
+dev_keychain_label="eumg-vmam-nluz-ygto"
+
+
+# put your project's information into these variables
+version="1.0.0"
+identifier="com.hanssak.OpenNetLinkApp"
+productname="OpenNetLinkApp"
+
+
+# code starts here
+
+projectdir=$(dirname $0)
+
+builddir="$projectdir/BUILD"
+pkgroot="$builddir/PKGROOT"
+
+ENT_PATH="$builddir/entitlements.plist"
+APP_PATH="$pkgroot/$productname.app"
+ZIP_PATH="$pkgroot/$productname.zip"
+
+# functions
+codesignapp() { # $1: path to file to codesign, $2: certification
+    apppath=${1:?"need a apppath"}
+    certificate=${2:?"need an Apple Development: FirstName LastName (XXXXXXXXXX)"}
+    entitlements=${3:?"need an entitlements.plist file full path)"}
+
+    # codesign --options=runtime -vvv --force --deep --sign "$certificate" "$apppath"
+    codesign -vvv --force --deep --entitlements $entitlements --options=runtime --sign "$certificate" "$apppath"
+    echo "Result codesign app: $apppath"
+    codesign -dv --verbose=4 "$apppath"
+}
+
+requeststatus() { # $1: requestUUID
+                              #--password "@keychain:$dev_keychain_label" 2>&1 \
+    requestUUID=${1?:"need a request UUID"}
+    req_status=$(xcrun altool --notarization-info "$requestUUID" \
+                              --username "$dev_account" \
+                              --password "$dev_keychain_label" 2>&1 \
+                 | awk -F ': ' '/Status:/ { print $2; }' )
+    echo "$req_status"
+}
+
+notarizefile() { # $1: path to file to notarize, $2: identifier
+    filepath=${1:?"need a filepath"}
+    identifier=${2:?"need an identifier"}
+    
+    # upload file
+    echo "## uploading $filepath for notarization"
+                               #--password "@keychain:$dev_keychain_label" \
+    requestUUID=$(xcrun altool --notarize-app \
+                               --primary-bundle-id "$identifier" \
+                               --username "$dev_account" \
+                               --password "$dev_keychain_label" \
+                               --asc-provider "$dev_team" \
+                               --file "$filepath" 2>&1 \
+                  | awk '/RequestUUID/ { print $NF; }')
+                               
+    echo "Notarization RequestUUID: $requestUUID"
+    
+    if [[ $requestUUID == "" ]]; then 
+        echo "could not upload for notarization"
+        exit 1
+    fi
+        
+    # wait for status to be not "in progress" any more
+    request_status="in progress"
+    while [[ "$request_status" == "in progress" ]]; do
+        echo -n "waiting... "
+        sleep 10
+        request_status=$(requeststatus "$requestUUID")
+        echo "$request_status"
+    done
+    
+    # print status information
+                 # --password "@keychain:$dev_keychain_label"
+    xcrun altool --notarization-info "$requestUUID" \
+                 --username "$dev_account" \
+                 --password "$dev_keychain_label"
+    echo 
+    
+    if [[ $request_status != "success" ]]; then
+        echo "## could not notarize $filepath"
+        exit 1
+    fi
+    
+}
+
+
+# build clean install
+
+# echo "## building with Xcode"
+# xcodebuild clean install -quiet
+
+
+# check if pkgroot exists where we expect it
+if [[ ! -d $pkgroot ]]; then
+    echo "couldn't find pkgroot $pkgroot"
+    exit 1
+fi
+
+## make codesign to app
+
+echo "## make codesign app: $APP_PATH"
+
+codesignapp "$APP_PATH" "$appledevsig" "$ENT_PATH"
+
+## make the zip
+# Create a ZIP archive suitable for altool.
+
+# echo "## building zip: $ZIP_PATH"
+
+# /usr/bin/ditto -c -k --keepParent "$APP_PATH" "$ZIP_PATH"
+
+# As a convenience, open the export folder in Finder.
+# /usr/bin/open "$pkgroot"
+
+## build the pkg
+
+pkgpath="$builddir/$productname-$version.pkg"
+
+echo "## building pkg: $pkgpath"
+
+pkgbuild --root "$pkgroot" \
+         --version "$version" \
+         --install-location "/Applications" \
+         --identifier "$identifier" \
+         --sign "$signature" \
+         "$pkgpath"
+
+# upload for notarization
+notarizefile "$pkgpath" "$identifier"
+# notarizefile "$ZIP_PATH" "$identifier"
+
+# staple result
+echo "## Stapling $pkgpath"
+xcrun stapler staple "$pkgpath"
+
+echo '## Done!'
+
+# show the pkg in Finder
+open -R "$pkgpath"
+
+exit 0
