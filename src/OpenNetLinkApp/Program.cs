9d73e1d0b6b594b75978279ebd9c7859f3dbac78
leeys <leeys@hanssak.co.kr>
2021-12-27 13:39:15 +0900

    FIX : OS별 cwd 경로 지정

--- src/OpenNetLinkApp/Program.cs
+++ src/OpenNetLinkApp/Program.cs
@@ -1,6 +1,7 @@
-﻿using WebWindows.Blazor;
+using WebWindows.Blazor;
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace OpenNetLinkApp
 {
@@ -8,9 +9,20 @@ namespace OpenNetLinkApp
     {
         static void Main(string[] args)
         {
+            string cwdPath = "";
             string CWD = Directory.GetCurrentDirectory();
             Console.WriteLine("==> {0}", CWD);
-            Directory.SetCurrentDirectory("/Applications/OpenNetLinkApp.app/Contents/MacOS");
+
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                cwdPath = "/Applications/OpenNetLinkApp.app/Contents/MacOS";
+            }
+            else 
+            {
+                cwdPath = Environment.CurrentDirectory;
+            }
+
+            Directory.SetCurrentDirectory(cwdPath);
             ComponentsDesktop.Run<Startup>("OpenNetLink", "wwwroot/index.html");
         }
     }
