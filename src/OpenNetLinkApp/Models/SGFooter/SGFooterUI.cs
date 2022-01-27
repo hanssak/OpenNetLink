24d5a6e584a4df18cf0d5bfb45c49d3b0b8c1d7b
silencep97 <silencep97@hanssak.co.kr>
2022-01-27 18:30:32 +0900

    [BASE] 상호 및 라이센스 정보등 변경

--- src/OpenNetLinkApp/Models/SGFooter/SGFooterUI.cs
+++ src/OpenNetLinkApp/Models/SGFooter/SGFooterUI.cs
@@ -10,9 +10,9 @@ namespace OpenNetLinkApp.Models.SGFooter
             Address = new List<string>();
             Description = new List<string>();
             Copyright = new List<string>();
-            CorpName = "(주) 한싹시스템";
+            CorpName = "(주)한싹";
             //Address.Add("서울시 구로구 디지털로34길 27 401호-403호 (구로동, 대륭포스트타워 3차)");
-            Copyright.Add("COPYRIGHT 2020 HANSSAK. ALL RIGHTS RESERVED");
+            Copyright.Add("COPYRIGHT HANSSAK. ALL RIGHTS RESERVED");
         }
         public string       CorpName { get; private set;} = null;
         public List<string> Address { get; private set;} = null;
