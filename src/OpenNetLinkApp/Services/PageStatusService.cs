744004b750712d72734c6dd6f62c9f5790a565ea
yhkim <yhkim@hanssak.co.kr>
2022-11-08 18:07:48 +0900

    Revert "[사후결재 컨트롤 동작] - 기능 수정"
    This reverts commit aaccaf8a4e4a1c8b08450ba3b20dfbd7373761c9.

--- src/OpenNetLinkApp/Services/PageStatusService.cs
+++ src/OpenNetLinkApp/Services/PageStatusService.cs
@@ -448,8 +448,6 @@ namespace OpenNetLinkApp.Services
             PageStatusData.SNotiEvent = afterApprTime;
         }
 
-
-
         public DateTime GetAfterApprTime(int groupID)
         {
             //PageStatusData tmpData = null;
@@ -470,20 +468,6 @@ namespace OpenNetLinkApp.Services
             //}
             ////m_DicPageStatusData[groupID].SetAfterApprTimeEvent(afterApprTime);
             PageStatusData.SCommonNotiEvent = afterApprTime;
-        }
-
-        /// <summary>
-        ///  공통환경설정의 사후결재 컨트롤 체크여부 변경 노티 (공통환경설정 -> 파일전송)
-        /// </summary>
-        /// <param name="a"></param>
-        public void SetAfterApprControlCheckEvent(int groupID, AfterApprTimeEvent afterApprControlCheck)
-        {
-            PageStatusData tmpData = null;
-            if (m_DicPageStatusData.TryGetValue(groupID, out tmpData) != true)
-            {
-                return;
-            }
-            PageStatusData.SAfterApprControlCheckEvent = afterApprControlCheck;
         }
 
         public void SetDayFileAndClipMax(int groupID, Int64 fileMaxSize, int fileMaxCount, Int64 clipMaxSize, int clipMaxCount)
