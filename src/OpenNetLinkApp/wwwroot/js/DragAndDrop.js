8dc847fe7b1f97d6b6833444977499c4c2448b5b
hsckLeeys <leeys@hanssak.co.kr>
2023-02-07 15:35:31 +0900

    NetWork.json, Env 파일 패치를 덮어쓰는 로직 삭제, 추후 다시 살림 파일명 길이 제한시 팝업창에 전송취소 버튼 추가 폴더 선택 팝업창 z-index 수정

--- src/OpenNetLinkApp/wwwroot/js/DragAndDrop.js
+++ src/OpenNetLinkApp/wwwroot/js/DragAndDrop.js
@@ -677,11 +677,11 @@ window.openPopUp = (popUpId) => {
 	//여기 인덱스를 강제로 내리는것은 문제있는 코드인데 일단 정확한 사유를 몰라 그냥 둠 2021/03/08 YKH
 	//
 	if (popUpId == "PopUpLogIn" || popUpId == "GPKIPopUp" || popUpId == "modal-pwchange-sidebar" || popUpId == "modal-pwchangedefaultpw-sidebar"
-        || popUpId == "ProxyApprover" || popUpId == "ProxyApproverTreePopUp" || popUpId == "SecurityApproverSelectPopUp" || popUpId == "PopUpSelectClipType" ) {
+        || popUpId == "ProxyApprover" || popUpId == "ProxyApproverTreePopUp" || popUpId == "SecurityApproverSelectPopUp" || popUpId == "PopUpSelectClipType") {
 		$("#left-sidebar").css("z-index", 2202);
 		$("#main-nav").css("z-index", 2202);
     }
-    else if (popUpId == "HeaderUIApporveAfterAlert" || popUpId == "HeaderUIApporveAfterMyCountAlert" || popUpId == "HeaderUIUpdateStartAlert")
+    else if (popUpId == "HeaderUIApporveAfterAlert" || popUpId == "HeaderUIApporveAfterMyCountAlert" || popUpId == "HeaderUIUpdateStartAlert" || popUpId == "modal-selectreceivefolder")
 	{
 		$("#main-nav").css("z-index", 2203);
     }
