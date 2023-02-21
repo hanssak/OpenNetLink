8a9e9004fb7a2f89d99a9847acafc43525839370
centOS7 <hsrnd1@gmail.com>
2023-02-21 17:24:02 +0900

    [CentOS 7] - 마임체크에 필요한 so 파일 추가 및 DB유지 기능 추가
     - 패키지 설치 시 os라이브러리 경로에 libbz2 파일이 없는 경우
       해당 경로에 생성해서 마임체크가 정상적으로 이루어지도록 추가
     - 업데이트 후 기존 자동로그인 설정 등을 유지하도록 DB 파일 백업/복원

--- src/OpenNetLinkApp/Library/bin.Script/OpenNetLinkActionBeforeInstall.sh
+++ src/OpenNetLinkApp/Library/bin.Script/OpenNetLinkActionBeforeInstall.sh
@@ -6,6 +6,7 @@ PATH_BIN_SCRIPT=$PATH_OPENNETLINK/bin.Script
 PATH_APPLICATIONS=/usr/share/applications/
 PATH_NEMO=/usr/share/nemo/actions/
 
+# 업데이트 후 기존 자동로그인 설정을 유지하기 위해 nsi 파일 수정
 #Move Network.json, AppEnvSetting.json not to change thoes information
 NETWORK_FILE="$PATH_OPENNETLINK/wwwroot/conf/NetWork.json"
 if [ -e $NETWORK_FILE ] ; then
@@ -15,4 +16,14 @@ fi
 APPENV_FILE="$PATH_OPENNETLINK/wwwroot/conf/AppEnvSetting.json"
 if [ -e $APPENV_FILE ] ; then
 	cp ${APPENV_FILE} /tmp
+fi
+
+SGNOTIFY_FILE="$PATH_OPENNETLINK/wwwroot/db/SGNotifyDB.db"
+if [ -e $SGNOTIFY_FILE ] ; then
+	cp ${SGNOTIFY_FILE} /tmp
+fi
+
+SGSETTING_FILE="$PATH_OPENNETLINK/wwwroot/db/SGSettingsDB.db"
+if [ -e $SGSETTING_FILE ] ; then
+	cp ${SGSETTING_FILE} /tmp
 fi
