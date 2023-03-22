034ba958f4a38c700add9f22120f786f24612b7d
yhkim <yhkim@hanssak.co.kr>
2023-03-22 12:10:44 +0900

    [결재자선택 팝업 개선] - 대결재자 Search / Target 테이블 개선 - 2
    - 커밋 누락 추가

--- src/OpenNetLinkApp/wwwroot/js/DragAndDrop.js
+++ src/OpenNetLinkApp/wwwroot/js/DragAndDrop.js
@@ -968,13 +968,14 @@ window.addMouseDown = (message) => {
             addTrSelection(e.target.parentElement, 3);
             return;
         }
+        //대결재 row select 처리는 razor에서 처리하도록 변경
         //대결자 검색 팝업
-        if (e.target.parentElement.getAttribute('name') == "trItem4") {
+        //if (e.target.parentElement.getAttribute('name') == "trItem4") {
 
-            clearTrSelections();
-            addTrSelection(e.target.parentElement, 4);
-            return;
-        }
+        //    clearTrSelections();
+        //    addTrSelection(e.target.parentElement, 4);
+        //    return;
+        //}
         if (e.target.parentElement.getAttribute('name') == "trItem5") {
 
             clearTrSelections();
@@ -1018,13 +1019,14 @@ window.addMouseDown = (message) => {
             addTrTargetSelection(e.target.parentElement, 3);
             return;
         }
+        //대결재 row select 처리는 razor에서 처리하도록 변경
         //대결자 지정
-        if (e.target.parentElement.getAttribute('name') == "trSelect4") {
+        //if (e.target.parentElement.getAttribute('name') == "trSelect4") {
 
-            clearTrTargetSelections(true);
-            addTrTargetSelection(e.target.parentElement, 4);
-            return;
-        }
+        //    clearTrTargetSelections(true);
+        //    addTrTargetSelection(e.target.parentElement, 4);
+        //    return;
+        //}
         if (e.target.parentElement.getAttribute('name') == "trSelect5") {
 
             clearTrTargetSelections(true);
@@ -1244,8 +1246,9 @@ function addTrTargetSelection(item, index) {
         DotNet.invokeMethodAsync("OpenNetLinkApp", "ApproverTargetSelect2", item.getAttribute('value'));
     else if (index == 3)
         DotNet.invokeMethodAsync("OpenNetLinkApp", "ApproverTargetSelect3", item.getAttribute('value'));
-    else if (index == 4)
-        DotNet.invokeMethodAsync("OpenNetLinkApp", "ProxyTargetSelect", item.getAttribute('value'));
+    //대결재 row select 처리는 razor에서 처리하도록 변경
+    //else if (index == 4)
+    //    DotNet.invokeMethodAsync("OpenNetLinkApp", "ProxyTargetSelect", item.getAttribute('value'));
     else if (index == 5)
         DotNet.invokeMethodAsync("OpenNetLinkApp", "ProxyTargetSelect2", item.getAttribute('value'));
     else if (index == 6)
@@ -1280,8 +1283,9 @@ function addTrSelection(item, index) {
         DotNet.invokeMethodAsync("OpenNetLinkApp", "ApproverSearchSelect2", item.getAttribute('value'));
     else if (index == 3)
         DotNet.invokeMethodAsync("OpenNetLinkApp", "ApproverSearchSelect3", item.getAttribute('value'));
-    else if (index == 4)
-        DotNet.invokeMethodAsync("OpenNetLinkApp", "ProxySearchSelect", item.getAttribute('value'));
+    //대결재 row select 처리는 razor에서 처리하도록 변경
+    //else if (index == 4)
+    //    DotNet.invokeMethodAsync("OpenNetLinkApp", "ProxySearchSelect", item.getAttribute('value'));
     else if (index == 5)
         DotNet.invokeMethodAsync("OpenNetLinkApp", "ProxySearchSelect2", item.getAttribute('value'));
     else if (index == 6)
