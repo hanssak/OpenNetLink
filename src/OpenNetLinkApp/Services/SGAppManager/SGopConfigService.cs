baf7ddfec406094fc0fde46eff90bc146f533a1c
smk <smk@hanssak.co.kr>
2023-08-28 12:37:23 +0900

    OpConfigService 수정 - NetworkService 수정필요

--- src/OpenNetLinkApp/Services/SGAppManager/SGopConfigService.cs
+++ src/OpenNetLinkApp/Services/SGAppManager/SGopConfigService.cs
@@ -256,28 +256,7 @@ namespace OpenNetLinkApp.Services.SGAppManager
             HsLogDel hsLog = new HsLogDel();
             hsLog.Delete(7);    // 7일이전 Log들 삭제
 
-            string strNetworkFileName = "wwwroot/conf/NetWork.json";
-            string jsonString = File.ReadAllText(strNetworkFileName);
             List<ISGNetwork> listNetworks = SGNetworkService.NetWorkInfo;
-            using (JsonDocument document = JsonDocument.Parse(jsonString))
-            {
-                JsonElement root = document.RootElement;
-                JsonElement NetWorkElement = root.GetProperty("NETWORKS");
-                //JsonElement Element;
-                foreach (JsonElement netElement in NetWorkElement.EnumerateArray())
-                {
-                    SGNetwork sgNet = new SGNetwork();
-                    string strJsonElement = netElement.ToString();
-                    var options = new JsonSerializerOptions
-                    {
-                        ReadCommentHandling = JsonCommentHandling.Skip,
-                        AllowTrailingCommas = true,
-                        PropertyNameCaseInsensitive = true,
-                    };
-                    sgNet = JsonSerializer.Deserialize<SGNetwork>(strJsonElement, options);
-                    listNetworks.Add(sgNet);
-                }
-            }
 
             if (!Directory.Exists(Environment.CurrentDirectory + $"/wwwroot/conf"))
                 Directory.CreateDirectory(Environment.CurrentDirectory + $"/wwwroot/conf");
