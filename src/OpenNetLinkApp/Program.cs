87981552566a41f240809db8f392f47790b93627
smk <smk@hanssak.co.kr>
2023-10-25 10:02:36 +0900

    Path 가 지정이 안되어 있어서 HsText 를 불러올 수 없어 오류 발생 해결

--- src/OpenNetLinkApp/Program.cs
+++ src/OpenNetLinkApp/Program.cs
@@ -28,6 +28,22 @@ namespace OpenNetLinkApp
 
             bool flagMutex;
             System.Threading.Mutex m_hMutex = new System.Threading.Mutex(true, "OpenNetLink", out flagMutex);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+#if DEBUG
+                        cwdPath = Environment.CurrentDirectory;
+#else
+                cwdPath = "/Applications/OpenNetLinkApp.app/Contents/MacOS";
+#endif
+            }
+            else
+            {
+                //cwdPath = Environment.CurrentDirectory;
+                //관리자가 사용자를 지정하여 OpenNetLink 실행 시에도 정상적인 경로로 표시하기 위해 변경
+                cwdPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            }
+
+            Directory.SetCurrentDirectory(cwdPath);
 
             string windowTitle = Common.CsFunction.XmlConf.GetTitle("T_WINDOW_TITLE");
             if (String.IsNullOrEmpty(windowTitle))
@@ -39,22 +55,6 @@ namespace OpenNetLinkApp
             if (flagMutex)
 #endif
             {
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-#if DEBUG
-                        cwdPath = Environment.CurrentDirectory;
-#else
-                        cwdPath = "/Applications/OpenNetLinkApp.app/Contents/MacOS";
-#endif
-                }
-                else 
-                {
-                    //cwdPath = Environment.CurrentDirectory;
-                    //관리자가 사용자를 지정하여 OpenNetLink 실행 시에도 정상적인 경로로 표시하기 위해 변경
-                    cwdPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
-                }
-
-                Directory.SetCurrentDirectory(cwdPath);
                 object[] arg = new object[2];
                 arg[0] = Services.SGAppManager.SGAppConfigService.AppConfigInfo.bStartProgramReg;
                 arg[1] = Services.SGAppManager.SGAppConfigService.AppConfigInfo.bStartTrayMove;
@@ -69,9 +69,7 @@ namespace OpenNetLinkApp
                 {
                     PostMessage(nhWnd, 0x0400+0x0003, 0, 0);
                 }
-
             }
-
         }
     }
 }
