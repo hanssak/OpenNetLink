97ae3d859d88c10d56fb921bc910fc5c1fe467cd
kwkim <silencep97@hanssak.co.kr>
2024-01-12 17:47:22 +0900

    dac 설정으로 drm 암호화 적용하는 방법 추가 - SITE test 필요

--- src/OpenNetLinkApp/Data/SGDicData/DRM/SGSoftCampDRM.cs
+++ src/OpenNetLinkApp/Data/SGDicData/DRM/SGSoftCampDRM.cs
@@ -19,6 +19,19 @@ namespace OpenNetLinkApp.Data.SGDicData.DRM
         [DllImport(strSoftCampDrmLibName, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
         public static extern void DSCSAddDac(int nGuide, string grade);
 
+        [DllImport(strSoftCampDrmLibName, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int DSCheckDSAgent();
+
+        [DllImport(strSoftCampDrmLibName, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int DSCSIsEncryptedFile(string strFullFilePath);
+
+        [DllImport(strSoftCampDrmLibName, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int DSCSDacEncryptFile(string strFullFilePath);
+
+        
+
+
+
 
         public int DrmEncrypt(string source, string grade)
         {
@@ -46,6 +59,78 @@ namespace OpenNetLinkApp.Data.SGDicData.DRM
                 return false;
             }
         }
+
+        /// <summary>
+        /// DRM 사용할 수 있도록 DRM Agent가 Login 되어 있는지 유무
+        /// </summary>
+        /// <returns></returns>
+        public bool DrmAgentisLogin()
+        {
+
+            int nStatus = -1;
+
+            try
+            {
+                nStatus = DSCheckDSAgent();
+
+                if (nStatus == 0)
+                    Log.Logger.Here().Error($"DrmAgentisLogin, DS Client Agent is Not Alive! (Ret=0)");
+                else if (nStatus == 1)
+                    Log.Logger.Here().Error($"DrmAgentisLogin, DS Client Agent is logout Status! (Ret=1)");
+
+                return (nStatus == 2);
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Here().Error($"DrmAgentisLogin, Exception(MSG) : {ex.Message}, Status : {nStatus}");
+                return false;
+            }
+        }
+
+        public bool DrmisEncryptFile(string strFilePath)
+        {
+            int nStatus = -1;
+
+            try
+            {
+                nStatus = DSCSIsEncryptedFile(strFilePath);
+
+                if (nStatus == -1)
+                    Log.Logger.Here().Error($"DrmisEncryptFile, DRM CS module Load Failed : {strFilePath}");
+                else if (nStatus == 0)
+                    Log.Logger.Here().Error($"DrmisEncryptFile, Normal File : {strFilePath}");
+
+                return (nStatus == 1);
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Here().Error($"DrmisEncryptFile, Exception(MSG) : {ex.Message}, Status : {nStatus}");
+                return false;
+            }
+        }
+
+        public bool DrmEncryptUsingDac(string strFilePath)
+        {
+            int nStatus = -1;
+
+            try
+            {
+                nStatus = DSCSDacEncryptFile(strFilePath);
+
+                if (nStatus == 3)
+                    Log.Logger.Here().Error($"DrmEncryptUsingDac, DRM CS Out Status, Status : {nStatus}");
+                else if (nStatus == 0)
+                    Log.Logger.Here().Error($"DrmEncryptUsingDac, DRM EncryptFile Failed : {strFilePath}");
+
+                return (nStatus == 1);
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Here().Error($"DrmEncryptUsingDac, Exception(MSG) : {ex.Message}, Status : {nStatus}");
+                return false;
+            }
+        }
+
 
     }
 }
